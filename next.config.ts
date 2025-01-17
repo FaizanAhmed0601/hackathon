ecf72ef918575c95b6f14735e3d30fd9e4bd4ff1
Faizan Ahmed Khan <160280304+FaizanAhmed0601@users.noreply.github.com>
2025-01-17 11:22:12 +0500

    first commit

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+
+// next.config.ts
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Remove invalid options
+  reactStrictMode: true,
+};
+
+export default nextConfig;
---
+++ postcss.config.mjs
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/no-anonymous-default-export */
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
